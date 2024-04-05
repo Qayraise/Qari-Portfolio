3447a817bf7d87046302c50c128bde8b0dc7e283
Muhammad Qayraise Ali <97973903+Qayraise@users.noreply.github.com>
2024-04-06 04:52:00 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Muhammad Qayraise Ali",
+  title: "Data Analyst & Power BI Developer",
+  email: "paulbjorn011@gmail.com",
+  gitHub: "Qayraise",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "mqayraiseali",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
